1bda90c34d959c4b4dd51a939cd5948b81f18a1e
Mostafa <banqusli@infai.org>
2018-05-15 10:01:08 +0200

    fix dashboard controoler

--- db/migrate/20180515075605_create_dashboards.rb
+++
@@ -1,9 +0,0 @@
-class CreateDashboards < ActiveRecord::Migration[5.1]
-  def change
-    create_table :dashboards do |t|
-      t.string :index
-
-      t.timestamps
-    end
-  end
-end
--- test/factories/dashboards.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :dashboard do
-    index "MyString"
-  end
-end
