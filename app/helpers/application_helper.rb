ca4e32390ba3fe75ec20a49730d5844f5355e5fe
Mostafa <banqusli@infai.org>
2018-05-16 11:00:41 +0200

    fix the association in form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-
-  def user_id?
-    @signed_in_user = session[:user_id]
-    @user = @signed_in_user.nil? ? @signed_in_user : current_user.id
-  end
-
-  def set_user
-    @id = current_user.user
-    @user = @id.nil? ? current_user : current_user.id
-  end
+  
 end
 
