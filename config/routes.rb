0a5a30d6a813463a200cdabcb9681162fcf2ef66
Mostafa <banqusli@infai.org>
2018-05-29 13:46:49 +0200

    set rout forlike button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :pictures
+  resources :pictures do
+    member do
+      put 'like', to: 'pictures#upvote'
+    end
+  end
   resources :selfie_users
   get 'index', to: 'dashboard#index'
   root 'dashboard#index'
