87bbc2b564a22a501bc87909b639fe89030d25c1
Mostafa <banqusli@infai.org>
2018-06-05 09:04:42 +0200

    i guss i have finshed styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    attributes = [:name, :email]
+    #devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
-
 end
