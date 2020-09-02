f2d1a3fb53cd0040577910a23bfe577fc58fa385
Moustafa Banqusli <banqusli@infai.org>
2020-09-02 17:22:01 +0200

    route to pictures dirct after sign in and fix flash error msg

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'error', to: 'dashboard#error'
   get 'index', to: 'dashboard#index'
   get 'index/id', to: 'dashboard#index#id'
-  root 'dashboard#index'
+  root 'pictures#index'
   devise_for :users
   mount Commontator::Engine => '/commontator'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
